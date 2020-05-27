a27edbdce72719115187b21470ebff1f8435d592
kevin3251 <herry3251@gmail.com>
2020-05-27 18:40:00 +0800

    update

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { render } from 'react-dom'
-import Grid from './components/Grid'
+import Grid from '../build/Grid'
 
 const board = {
   layout: {
--- webpack.config.js
+++ webpack.config.js
@@ -2,5 +2,9 @@
 // changes is via the presets' options or Neutrino's API in `.neutrinorc.js` instead.
 // Neutrino's inspect feature can be used to view/export the generated configuration.
 const neutrino = require('neutrino');
+let config = neutrino().webpack();
 
-module.exports = neutrino().webpack();
+config.output.libraryTarget = 'var'
+config.output.umdNamedDefine = false
+
+module.exports = config
