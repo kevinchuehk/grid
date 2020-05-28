96635516166ccd8efde54c99e793d0f7ce85ed09
kevin3251 <herry3251@gmail.com>
2020-05-28 17:25:12 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -3,18 +3,16 @@
 // Neutrino's inspect feature can be used to view/export the generated configuration.
 const neutrino = require('neutrino');
 const path = require('path')
-const EsmWebpackPlugin = require("@purtuga/esm-webpack-plugin");
 let config = neutrino().webpack();
 
-config.output.library = "LIB"
+// config.output.library = "LIB"
 config.output.path = path.resolve(__dirname, 'dist/umd')
 
 let cjs = {
     ...config,
     output: {
         filename: '[name].js',
-        library: 'LIB',
-        libraryTarget: 'commonjs',
+        libraryTarget: 'commonjs2',
         path: path.resolve(__dirname, 'dist/cjs')
     }
 }
