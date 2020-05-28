fcae7d2e63f7ee2cd9c517a8aa47eab230e4e377
kevin3251 <herry3251@gmail.com>
2020-05-28 10:45:10 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,12 @@
 // changes is via the presets' options or Neutrino's API in `.neutrinorc.js` instead.
 // Neutrino's inspect feature can be used to view/export the generated configuration.
 const neutrino = require('neutrino');
+const EsmWebpackPlugin = require("@purtuga/esm-webpack-plugin");
 let config = neutrino().webpack();
 
+config.output.library = "LIB"
 config.output.libraryTarget = 'var'
 config.output.umdNamedDefine = false
+config.plugins = [ ...config.plugins, new EsmWebpackPlugin() ]
 
 module.exports = config
